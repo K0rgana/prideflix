1f9b9df7772c5b7bc65967b8fee5b7c21ec1b3fa
K0rgana <anagrom1999@gmail.com>
2020-08-01 15:25:30 -0300

    atualizando url de apresentação dos dados

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -32,7 +32,7 @@ function CadastroCategoria() {
 
   useEffect(() => {
     if (window.location.href.includes('localhost')) {
-      const URL = 'http://localhost:8080/categorias';
+      const URL = 'https://prideflix.herokuapp.com/categorias';
       fetch(URL)
         .then(async (respostaDoServer) => {
           if (respostaDoServer.ok) {
