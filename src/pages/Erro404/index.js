4734e6e420a187f05f4e63d19bc26759cad32260
K0rgana <anagrom1999@gmail.com>
2020-08-03 12:32:25 -0300

    corrigindo página 404

--- src/pages/Erro404/index.js
+++ src/pages/Erro404/index.js
@@ -3,14 +3,17 @@ import { Link } from 'react-router-dom';
 import PageDefault from '../../components/PageDefault';
 
 function Erro404() {
-    return (
-      <PageDefault>
-        <Link to="/">
-            <img  src="https://blog.jovempan.com.br/paulacarvalho/wp-content/uploads/sites/14/2018/02/pabblo.gif" alt="pabllo Vittar correndo no deserto" />
-        </Link>
-        <h1>Yukê! Você foi longe demais</h1>
-        <h3>Não conseguimos entender onde você quer chegar</h3>
-      </PageDefault>
-    );
-  }
-  export default Erro404;
+  return (
+    <PageDefault>
+      <h1>Yukê! Você foi longe demais</h1>
+
+      <Link to="/">
+        <img src="https://blog.jovempan.com.br/paulacarvalho/wp-content/uploads/sites/14/2018/02/pabblo.gif" alt="pabllo Vittar correndo no deserto" />
+      </Link>
+
+      <h3>Não conseguimos entender onde você quer chegar</h3>
+      <h3>Erro 404 - Não Encontrado</h3>
+    </PageDefault>
+  );
+}
+export default Erro404;
